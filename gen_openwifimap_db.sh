13d9dc9989f0ee50bfc46b857ea8fd4e3cc524e1
Tod Fitch <Tod@FitchDesign.com>
2014-12-06 11:34:54 -0800

    Add filtering by lat,lon to gen script
    Signed-off-by: Tod Fitch <Tod@FitchDesign.com>

--- gen_openwifimap_db.sh
+++ gen_openwifimap_db.sh
@@ -5,10 +5,38 @@
 #   OpenWlanMapNlpBackend.
 #
 
+MAX_LAT="90"
+MIN_LAT="-90"
+MAX_LON="180"
+MIN_LON="-180"
+
+function usage {
+    echo "Calling Sequence:"
+    echo "${0} [options]"
+    echo "  Options:"
+    echo "      -nDD    -(North) Maximum latitude"
+    echo "      -sDD    -(South) Minimum latitude"
+    echo "      -eDD    -(East) Maximum longitude"
+    echo "      -nDD    -(West) Minimum latitude"
+   exit
+}
+
+#Process the arguments
+while getopts n:s:e:w: opt
+do
+   case "$opt" in
+      n) MAX_LAT=$OPTARG;;
+      s) MIN_LAT=$OPTARG;;
+      e) MAX_LON=$OPTARG;;
+      w) MIN_LON=$OPTARG;;
+      \?) usage;;
+   esac
+done
 
 #
 #   Get latest wifi AP locations from OpenWLANMap.org
 #
+
 echo 'Getting wifi AP locations from OpenWLANMap.org'
 if [ -e db.tar.bz2 ] ; then
     rm db.tar.bz2
@@ -18,7 +46,6 @@ if [ -e db.csv ] ; then
 fi
 wget "http://openwlanmap.org/db.tar.bz2"
 tar --strip-components=1 -xjf db.tar.bz2 db/db.csv
-
 
 echo 'Building database file'
 if [ -e openwifimap.db ] ; then
@@ -33,13 +60,17 @@ CREATE TABLE APs(bssid STRING, latitude REAL, longitude REAL);
 .mode csv
 .separator "\t"
 .import db.csv APs
+DELETE FROM APs WHERE latitude>${MAX_LAT};
+DELETE FROM APs WHERE latitude<${MIN_LAT};
+DELETE FROM APs WHERE longitude>${MAX_LON};
+DELETE FROM APs WHERE longitude<${MIN_LON};
 CREATE INDEX _idx1 ON APs (bssid);
 VACUUM;
 .quit
 !
 
 #
-#   Push the new database to the phone. 
+#   Push the new database to the phone.
 #
 echo 'Pushing database to phone'
 adb push openwifimap.db /sdcard/.nogapps/openwifimap.db
